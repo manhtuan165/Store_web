7f9dc6a3e99951d6921d525dff8b26c28be45600
manhtuan165 <manhtuan165@gmail.com>
2016-09-13 01:25:33 +0700

    130916

---
+++ TeduShop.Web/Api/PostCategoryController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+using TeduShop.Web.Infrastructure.Core;
+
+namespace TeduShop.Web.Api
+{
+    [RoutePrefix("api/postcategory")]
+    public class PostCategoryController : ApiControllerBase
+    {
+        IPostCategoryService _postCategoryService;
+        public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService)
+            : base(errorService)
+        {
+            this._postCategoryService = postCategoryService;
+        }
+
+        //select
+        [Route("getall")]
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    response = request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var lstcategory = _postCategoryService.GetAll();
+                    response = request.CreateResponse(HttpStatusCode.OK, lstcategory);
+                }
+                return response;
+            });
+        }
+
+
+        //create
+        public HttpResponseMessage Post(HttpRequestMessage request, PostCategory postCategory)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    response = request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var category = _postCategoryService.Add(postCategory);
+                    _postCategoryService.Save();
+                    response = request.CreateResponse(HttpStatusCode.Created, category);
+                }
+                return response;
+            });
+        }
+
+        //Update
+        public HttpResponseMessage Put(HttpRequestMessage request, PostCategory postCategory)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    response = request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _postCategoryService.Update(postCategory);
+                    _postCategoryService.Save();
+                    response = request.CreateResponse(HttpStatusCode.OK);
+                }
+                return response;
+            });
+        }
+
+        public HttpResponseMessage Delete(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    response = request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _postCategoryService.Delete(id);
+                    _postCategoryService.Save();
+                    response = request.CreateResponse(HttpStatusCode.OK);
+                }
+                return response;
+            });
+        }
+    }
+}
---
+++ TeduShop.Web/Infrastructure/Core/ApiControllerBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+
+namespace TeduShop.Web.Infrastructure.Core
+{
+    public class ApiControllerBase : ApiController
+    {
+        private IErrorService _errorService;
+        public ApiControllerBase(IErrorService errorService)
+        {
+            this._errorService = errorService;
+        }
+
+        protected HttpResponseMessage CreateHttpResponse(HttpRequestMessage requestMessage, Func<HttpResponseMessage> function)
+        {
+            HttpResponseMessage response = null;
+            try
+            {
+                response = function.Invoke();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                foreach (var eve in ex.EntityValidationErrors)
+                {
+                    Trace.WriteLine(string.Format("Entity of type \"{0}\" in state \"{1}\" has the following validation error.", eve.Entry.Entity.GetType().Name, eve.Entry.State));
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Trace.WriteLine(string.Format("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage));
+                    }
+                }
+                LogError(ex);
+                response = requestMessage.CreateResponse(HttpStatusCode.BadRequest, ex.InnerException.Message);
+            }
+            catch (DbUpdateException dbEx)
+            {
+                LogError(dbEx);
+                response = requestMessage.CreateResponse(HttpStatusCode.BadRequest, dbEx.InnerException.Message);
+            }
+
+            catch (Exception ex)
+            {
+                LogError(ex);
+                response = requestMessage.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+
+            }
+            return response;
+        }
+
+        private void LogError(Exception ex){
+            try 
+	        {	        
+		        Error error = new Error();
+                error.CreateDate = DateTime.Now;
+                error.Message = ex.Message;
+                error.StackTrace = ex.StackTrace;
+                _errorService.Create(error);
+                _errorService.Save();
+
+	        }
+	        catch
+	        {
+		
+	        } 
+        }
+    }
+}
